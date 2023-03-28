9e72c1e8963e56727e97f1f90779d0ea48e4d07b
unknown <mashamsaiprabhath44@gmail.com>
2023-03-28 11:34:06 +0530

    initial commit

--- app.py
+++ app.py
@@ -9,8 +9,6 @@ with open(model_file, 'rb') as f_in:
 
 
 def main():
-
-	image = Image.open('images/icone.png')
 	image2 = Image.open('images/image.png')
 	st.image(image,use_column_width=False)
 	add_selectbox = st.sidebar.selectbox(
